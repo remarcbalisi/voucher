3ce7586d29f352b722ee29abff51aa421941ff0e
Remarc Balisi <remarc.balisi@gmail.com>
2023-06-18 01:38:26 +0800

    added user model

---
+++ techfarmlink/api/model/user.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+import uuid
+
+from bson import ObjectId
+from marshmallow import Schema, fields, post_load, pre_load
+
+
+class User(object):
+    def __init__(self, name, email, phone_number, password, status, _id=None, email_verified=False, phone_number_verified=False, created_at=None):
+        self._id = _id or uuid.uuid4().hex
+        self.name = name
+        self.email = email
+        self.email_verified = email_verified
+        self.phone_number = phone_number
+        self.phone_number_verified = phone_number_verified
+        self.password = password
+        self.created_at = created_at or datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        self.status = status
+
+    def __repr__(self):
+        return '<User(name={self.name!r})>'.format(self=self)
+
+
+class UserSchema(Schema):
+    _id = fields.Str()
+    name = fields.Str()
+    email = fields.Str()
+    email_verified = fields.Boolean()
+    phone_number = fields.Str()
+    phone_number_verified = fields.Boolean()
+    password = fields.Str()
+    created_at = fields.DateTime()
+    status = fields.Str()
+
+    @post_load
+    def make_store(self, data, **kwargs):
+        return User(**data)
+
+    @pre_load
+    def pre_process_details(self, data, **kwarg):
+        data['_id'] = str(data['_id']) if isinstance(data['_id'], ObjectId) else data['_id']
+        data['created_at'] = data['created_at'].strftime("%Y-%m-%d %H:%M:%S") if isinstance(data['created_at'], datetime) else data['created_at']
+        return data
